d8933e05ba203fc7232b0dfaefcb1ece266e3cf2
Rohit Raut <rohit@Rohits-MacBook-Pro.local>
2022-04-02 19:34:49 +0530

    Test husky change

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-                <p>
-                  <button type="button" onClick={() => setCount((count) => count + 1)}>
-                    count is: {count}
-                  </button>
-                </p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
